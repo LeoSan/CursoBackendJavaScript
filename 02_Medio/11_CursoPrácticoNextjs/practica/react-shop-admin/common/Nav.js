afbeaf7df63f9fc857d4e0957103ea580afa4732
LeoSan <cuenca623@gmail.com>
2022-07-01 00:44:30 -0500

    Clase 19 - Modal

--- 02_Medio/11_CursoPrácticoNextjs/practica/react-shop-admin/common/Nav.js
+++ 02_Medio/11_CursoPrácticoNextjs/practica/react-shop-admin/common/Nav.js
@@ -7,7 +7,7 @@ export default function Nav() {
   return (
     <nav className="bg-white shadow">
       <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold text-gray-900 capitalize">{route}</h1>
+        <h1 className="text-sm font-bold text-gray-900 capitalize py-2">{route}</h1>
       </div>
     </nav>
   );
